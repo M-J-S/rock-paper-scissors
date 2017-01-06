68feb8211ba7cccf3e59d365e4453959eb8ed65e
Matthew Sarantos <M-J-S@users.noreply.github.com>
2017-01-05 23:45:27 -0700

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,11 +1,13 @@
 from random import randrange
 
-#rock, paper, scissors instructions 
+#rock, paper, scissors instructions
+print("------------------------------------------------------------------")
 print("rock, paper, scissors!")
-print("r = rock")
+print("\nr = rock")
 print("p = paper")
 print("s = scissors")
 print("q = quit")
+print("------------------------------------------------------------------")
 
 #computer's random choice (Can be either 1, 2, or 3)
 computerChoice = randrange(1, 4)
@@ -20,29 +22,38 @@ while playerChoice != 'q':
     if (playerChoice == 'r'):
         playerNumber = 1
         if (computerChoice == 1):
-            print("tie")
+            print("Computer chose rock")
+            print("tie, play again? y/n")
         elif (computerChoice == 2):
-            print("you lose")
+            print("Computer chose paper")
+            print("you lose, play again? y/n")
         else:
-            print("you win")
+            print("Computer chose scissors")
+            print("you win, play again? y/n")
         break
     elif (playerChoice == 'p'):
         playerNumber = 2
         if (computerChoice == 1):
-            print("you win")
+            print("Computer chose rock")
+            print("you win, play again? y/n")
         elif (computerChoice == 2):
-            print("tie")
+            print("Computer chose paper")
+            print("tie, play again? y/n")
         else:
-            print("you lose")
+            print("Computer chose scissors")
+            print("you lose, play again? y/n")
         break
     elif (playerChoice == 's'):
         playerNumber = 3
         if (computerChoice == 1):
-            print("you lose")
+            print("Computer chose rock")
+            print("you lose, play again? y/n")
         elif (computerChoice == 2):
-            print("you win")
+            print("Computer chose paper")
+            print("you win, play again? y/n")
         else:
-            print("tie")
+            print("Computer chose scissors")
+            print("tie, play again? y/n")
         break
     else:
         print("Enter a valid input")

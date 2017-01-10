54dc6675bc3e539f83ed44ad1f4eba4cbaa9049b
Matthew Sarantos <M-J-S@users.noreply.github.com>
2017-01-09 20:41:45 -0700

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,26 +1,30 @@
 from random import randrange
 
-#rock, paper, scissors instructions
-print("------------------------------------------------------------------")
-print("rock, paper, scissors!")
-print("\nr = rock")
-print("p = paper")
-print("s = scissors")
-print("q = quit")
-print("------------------------------------------------------------------")
-
-#computer's random choice (Can be either 1, 2, or 3)
-computerChoice = randrange(1, 4)
-
 #global variables to keep track of score
 computerScore = 0
 playerScore = 0
-
-
-#Takes input from player and then lowercases incase player enters a capital letter
-playerChoice = input("").lower()
+playerChoice = ''
+yesOrNo = ''
 
 while playerChoice != 'q':
+    #rock, paper, scissors instructions
+    print("------------------------------------------------------------------")
+    print("rock, paper, scissors!")
+    print("\nr = rock")
+    print("p = paper")
+    print("s = scissors")
+    print("q = quit")
+    print("------------------------------------------------------------------")
+
+    #computer's random choice (Can be either 1, 2, or 3)
+    computerChoice = randrange(1, 4)
+
+    #Takes input from player and then lowercases incase player enters a capital letter
+    playerChoice = input("").lower()
+
+    while (playerChoice != 'r' and playerChoice != 'p' and playerChoice != 's' and playerChoice != 'q'):
+        print("Enter a valid input")
+        playerChoice = input("").lower()
 
     #converts player's choice to a number to make comparing to the computer's choice easier or tells player their selection isn't valid
     #player chooses rock and 3 options for whatever computer chooses
@@ -31,21 +35,31 @@ while playerChoice != 'q':
             print("tie, score:")
             print("Computer: " + str(computerScore))
             print("You: " + str(playerScore))
-            print("Play again? y/n")
         elif (computerChoice == 2):
             print("Computer chose paper")
             computerScore += 1
             print("you lose, score:")
             print("Computer: " + str(computerScore))
             print("You: " + str(playerScore))
-            print("Play again? y/n")
         else:
             print("Computer chose scissors")
             playerScore += 1
             print("you win, score:")
             print("Computer: " + str(computerScore))
             print("You: " + str(playerScore))
-            print("Play again? y/n")
+        print("Play again? y/n")
+        yesOrNo = input("").lower()
+        
+        while (yesOrNo != 'n' and yesOrNo != 'y'):
+            print("Enter a valid input")
+            yesOrNo = input("").lower()
+
+        if (yesOrNo == 'y'):
+            print("new game")
+        if (yesOrNo == 'n'):
+            playerChoice == 'q'
+            break
+        
             
     #player chooses paper and 3 options for whatever computer chooses
     elif (playerChoice == 'p'):
@@ -56,20 +70,31 @@ while playerChoice != 'q':
             print("you win, score:")
             print("Computer: " + str(computerScore))
             print("You: " + str(playerScore))
-            print("Play again? y/n")
         elif (computerChoice == 2):
             print("Computer chose paper")
             print("tie, score:")
             print("Computer: " + str(computerScore))
             print("You: " + str(playerScore))
-            print("Play again? y/n")
         else:
             print("Computer chose scissors")
             computerScore += 1
             print("you lose, score:")
             print("Computer: " + str(computerScore))
             print("You: " + str(playerScore))
-            print("Play again? y/n")
+            
+        print("Play again? y/n")
+        yesOrNo = input("").lower()
+        
+        while (yesOrNo != 'n' and yesOrNo != 'y'):
+            print("Enter a valid input")
+            yesOrNo = input("").lower()
+
+        if (yesOrNo == 'y'):
+            print("new game")
+        if (yesOrNo == 'n'):
+            playerChoice == 'q'
+            break
+        
             
     #player chooses scissors and 3 options for whatever computer chooses
     elif (playerChoice == 's'):
@@ -80,45 +105,35 @@ while playerChoice != 'q':
             print("you lose, score:")
             print("Computer: " + str(computerScore))
             print("You: " + str(playerScore))
-            print("Play again? y/n")
         elif (computerChoice == 2):
             print("Computer chose paper")
             playerScore += 1
             print("you win, score:")
             print("Computer: " + str(computerScore))
             print("You: " + str(playerScore))
-            print("Play again? y/n")
         else:
             print("Computer chose scissors")
             print("tie, score:")
             print("Computer: " + str(computerScore))
             print("You: " + str(playerScore))
-            print("Play again? y/n")
-    else:
-        print("Enter a valid input")
-        playerChoice = input("").lower()
+            
+        print("Play again? y/n")
+        yesOrNo = input("").lower()
+        
+        while (yesOrNo != 'n' and yesOrNo != 'y'):
+            print("Enter a valid input")
+            yesOrNo = input("").lower()
 
-    #takes user input of either y/n
-    yesOrNo = input("").lower()
-    if (yesOrNo == 'n'):
-        break
-    else:
-        #rock, paper, scissors instructions
-        print("------------------------------------------------------------------")
-        print("rock, paper, scissors!")
-        print("\nr = rock")
-        print("p = paper")
-        print("s = scissors")
-        print("q = quit")
-        print("------------------------------------------------------------------")
-
-        #computer's random choice (Can be either 1, 2, or 3)
-        computerChoice = randrange(1, 4)
-
-
-        #Takes input from player and then lowercases incase player enters a capital letter
-        playerChoice = input("").lower()
-
+        if (yesOrNo == 'y'):
+            print("new game")
+        if (yesOrNo == 'n'):
+            playerChoice == 'q'
+            break
+        
+    elif (playerChoice == 'q'):
+        print("Final Score:")
+        print("Computer: " + str(computerScore))
+        print("You: " + str(playerScore))
 
 
 

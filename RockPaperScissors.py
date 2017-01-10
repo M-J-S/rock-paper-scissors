89c5a5e07608ea92ea417b58dfb7d48ee0e37cfd
Matthew Sarantos <M-J-S@users.noreply.github.com>
2017-01-09 23:59:32 -0700

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -30,21 +30,25 @@ while playerChoice != 'q':
     playerChoice = input("")
 
 
+    #while the user doesn't choose a valid choice from the menu the program loops to ask for a valid input
     while (playerChoice != 'r' and playerChoice != 'p' and playerChoice != 's' and playerChoice != 'q'):
         print("Enter a valid input")
         playerChoice = input("")
 
+    #if the user inputs q the program exits the main loop to stop after printing the final score
     if (playerChoice == 'q'):
         print("Final Score:")
         print("Computer: " + str(computerScore))
         print("You: " + str(playerScore))
-        
+
+    #if the player's choice of r,p,s = RPS[1:r, 2:p, 3:p] then both players get a point and Tie! is displayed
     elif(str(playerChoice) == RPS[str(computerChoice)]):
         computerScore += 1
         playerScore += 1
         print("Computer chose " + RPS[str(computerChoice)])
         print("Tie!")
 
+    #if  the player's choice of RPS[r:3, p:1, s:2]
     elif(RPS[playerChoice] == str(computerChoice)):
         playerScore += 1
         print("Computer chose " + RPS[str(computerChoice)])
